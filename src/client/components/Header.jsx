ff413cdb36ddb90436246c1dd0ecec806e94316e
Oliver Trinidad <ocmtrinidad@gmail.com>
2024-05-22 13:56:45 +0800

    Refactored <Header />. <TimeKeeper /> now called in <Header />

--- src/client/components/Header.jsx
+++ src/client/components/Header.jsx
@@ -1,13 +1,25 @@
 import Timer from "./Timer";
+import TimeKeeper from "./TimeKeeper";
+import { useEffect } from "react";
 import "../stylesheets/Header.css";
 
 const Header = (props) => {
+  useEffect(() => {
+    const interval = setInterval(() => {
+      props.setTime((t) => t + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <header>
       <h1>Level {props.level + 1}</h1>
       <div>
-        <p>Time:</p>
-        <Timer time={props.time} setTime={props.setTime} />
+        <p>Time: </p>
+        <TimeKeeper time={props.time} />
       </div>
     </header>
   );
--- src/client/components/Timer.jsx
+++ src/client/components/Timer.jsx
@@ -1,19 +1,19 @@
-import { useEffect, useState } from "react";
-import "../stylesheets/Timer.css";
-import TimeKeeper from "./TimeKeeper";
+// import { useEffect, useState } from "react";
+// import "../stylesheets/Timer.css";
+// import TimeKeeper from "./TimeKeeper";
 
-const Timer = (props) => {
-  useEffect(() => {
-    const interval = setInterval(() => {
-      props.setTime((t) => t + 1);
-    }, 1000);
+// const Timer = (props) => {
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       props.setTime((t) => t + 1);
+//     }, 1000);
 
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
+//     return () => {
+//       clearInterval(interval);
+//     };
+//   }, []);
 
-  return <TimeKeeper time={props.time} />;
-};
+//   return <TimeKeeper time={props.time} />;
+// };
 
-export default Timer;
+// export default Timer;
